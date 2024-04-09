d69f2832de2474b8d14fb551d28156d2e004214c
NandiniAccolite <nandini.katara@accolitedigital.com>
2024-04-09 21:56:33 +0530

    Initial commit

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AddTwoNumbers
+{
+    internal class Program
+    {
+        static double Addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition of Two Numbers : ");
+            Console.WriteLine("Enter first number : ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number : ");
+            double num2 = double.Parse(Console.ReadLine());
+            double sum = Addition(num1, num2);
+            Console.WriteLine($"Result : {num1} + {num2} = {sum}");
+
+        }
+    }
+}
